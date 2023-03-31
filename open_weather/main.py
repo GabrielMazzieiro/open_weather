840e28135d73e30cf990938722b8e70f6508739d
GabrielMazzieiro <gabrielpm94@gmail.com>
2023-03-31 15:24:27 -0300

    fix: :bug: fix import in main file

--- open_weather/main.py
+++ open_weather/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from src.db.connections import mongo_client
-from open_weather.open_weather.src.api.routes import api_router
+from src.api.routes import api_router
 
 app = FastAPI()
 app.include_router(api_router)
@@ -9,7 +9,3 @@ app.include_router(api_router)
 def read_root():
     return {"Status": "Ok"}
 
-@app.post("/create_database")
-def create_database():
-    mongo_client.teste_db.teste_collection.insert_one({"teste": "teste"})
-    return "ok"
